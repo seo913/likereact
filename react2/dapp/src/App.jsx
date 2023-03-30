de4238d853f492365e46e4bb6d1c97085ccaed68
서재민 <seojaemin@seojaemin-ui-MacBookPro.local>
2023-03-30 16:37:52 +0900

    portfolio header + body + projectcard

--- react2/dapp/src/App.jsx
+++ react2/dapp/src/App.jsx
@@ -17,11 +17,53 @@ function App() {
       </header>
       <main>
         <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
-          <div className="bg-blue-200 h-72 w-6/12 flex justify-center items-center">여기에 나를 소개하는 내요을 적으면 됩니다.</div>
-        </div>
+          여기에 나를 소개하는 내요을 적으면 됩니다.</div>
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* 이미지가 들어오면 object-cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">프로젝트 이미지</div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* 이미지가 들어오면 object-cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">프로젝트 이미지</div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* 이미지가 들어오면 object-cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">프로젝트 이미지</div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* 이미지가 들어오면 object-cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">프로젝트 이미지</div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* 이미지가 들어오면 object-cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">프로젝트 이미지</div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* 이미지가 들어오면 object-cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">프로젝트 이미지</div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
       </main>
     </div>
-  );
+  ); 
 }
 
 export default App;
