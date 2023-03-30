0fa5d58b23547bdc2e5009c8c4e7093604789b19
서재민 <seojaemin@seojaemin-ui-MacBookPro.local>
2023-03-30 15:44:40 +0900

    portfolio header

--- react2/dapp/src/App.jsx
+++ react2/dapp/src/App.jsx
@@ -1,10 +1,20 @@
+import TailwindLogo from "./images/tailwind.jpeg";
 
 function App() {
+  
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello, React!</h1>
-      <h2 className="text-3xl font-bold underline">My Name is sjm</h2>
-      <h3 className="text-3xl font-bold underline">My Favorite is Zero Coke </h3>
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-100 h-12 md:h-20" > 
+      <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+        <div>SJM Portfolio</div>
+        {/* <div><img className="w-20 md:w-40" src={TailwindLogo} alt="Tailwind"/></div> */}
+        <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
+          <li className="bg-indigo-300 btn-style">Introduce</li>
+          <li className="bg-purple-300 btn-style">Portfolio</li>
+          <li className="bg-green-300 btn-style">Contact Me</li>
+        </ul>
+      </div>
+      </header>
     </div>
   );
 }
