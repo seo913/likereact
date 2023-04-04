26e6b030a64bf4df15a9312729b2914432ca81b9
서재민 <seojaemin@seojaemin-ui-MacBookPro.local>
2023-04-04 11:19:18 +0900

    excard

---
+++ react8/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card ({symbol, score}) {
+    return(
+        <div className={`relative w-24 h-36 flex justify-center items-center border-2 rounded-lg  ${
+            symbol === "♠︎" || symbol ==="♣︎" ?"border-black text-black" : "border-red-500 text-red-500"   }`}>
+            <div className="absolute top-0 left-0 m-1">{symbol}</div>
+            <div>{score}</div>
+            <div className="absolute buttom-0 right-0 m-1 ">{symbol}</div>
+        </div>
+    )
+}
+
+export default Card;
